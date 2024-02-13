20114294e2fc6f7297b003886f833c60a071642b
catinrage <catinrage@gmail.com>
2024-02-14 01:28:58 +0330

    parser reimplemented

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,9 +1,9 @@
 import { Pattern, Token } from '../common';
-import { LiquidErrorInstance } from '../common/helpers';
 import type { Grammar } from '../common/types';
 
 type Node = {
-  payload: string;
+  value: string;
+  parent?: Node;
   children: Node[];
 };
 
@@ -253,29 +253,21 @@ export class Parser {
    * TODO: make this a pure function
    */
   public parse(input: Token[]): CST | boolean {
-    const parseTree = new CST({
-      payload: this.variables[0],
-      children: [],
-    });
-    const currentNode = parseTree.root;
-    const stack = ['EOF'];
-    let i = 0;
-    stack.push(this.variables[0]);
-    while (stack.length > 0) {
-      const token = input[i];
-      const top = stack.pop() as string;
-      if (
-        !this.variables.includes(token.type) &&
-        !this.terminals.includes(token.type) &&
-        token.type !== 'EOF'
-      ) {
-        if (!token.groups.some((group) => this.terminals.includes(`[${group}]`))) {
-          throw LiquidErrorInstance('Parser', `Undefined token \`${token.type}\``, token.start);
-        }
+    // we wan't to create parse tree here, and determine if grammar accepts the input array (recursive approach)
+    const build = (input: Token[], stack: string[]): boolean => {
+      if (input.length === 0) {
+        return stack.length === 0;
       }
+
+      const top = stack.pop() as string;
+      const token = input[0];
+
+      // console.log({ top, token });
+
+      // top is a variable
       if (this.variables.includes(top)) {
-        let reference = this.table[top][token.type];
-        if (reference === -1 || reference === undefined) {
+        let reference = this.table[top][token.type] ?? -1;
+        if (reference === -1) {
           for (const group of token.groups) {
             reference = this.table[top][`[${group}]`] ?? -1;
             if (reference !== -1) break;
@@ -283,31 +275,43 @@ export class Parser {
         }
         if (reference === -1) {
           return false;
-        } else {
-          const pn = reference;
-          const production = this.grammar[pn];
-          if (production.rhs[0] !== '_EPS_') stack.push(...production.rhs.toReversed());
-          for (const symbol of production.rhs) {
-            const newNode: Node = {
-              payload: symbol,
-              children: [],
-            };
-            currentNode.children.push(newNode);
+          // throw LiquidErrorInstance('Parser', `Invalid token \`${token.type}\``, token.start);
+        }
+
+        const production = this.grammar[reference];
+        stack = [...stack, ...production.rhs.filter((symbol) => symbol !== '_EPS_').toReversed()];
+
+        return build(structuredClone(input), structuredClone(stack));
+      }
+
+      // top is a terminal
+      if (top === token.type) {
+        input.shift();
+        return build(structuredClone(input), structuredClone(stack));
+      } else {
+        // top is a group name
+        if (/\[.*\]/.test(top)) {
+          let accepted = false;
+          for (const group of token.groups) {
+            if (top === `[${group}]`) {
+              accepted = true;
+              break;
+            }
+          }
+
+          if (accepted) {
+            input.shift();
+            return build(structuredClone(input), structuredClone(stack));
+          } else {
+            return false;
+            // throw LiquidErrorInstance('Parser', `Invalid token \`${token.type}\``, token.start);
           }
         }
-      } else {
-        if (top !== token.type && !token.groups.map((group) => `[${group}]`).includes(top)) {
-          throw LiquidErrorInstance('Parser', `Unexpected token \`${token.type}\``, token.start);
-        } else {
-          i++;
-          // add the token to the parse tree
-          currentNode.children.push({
-            payload: token.lexeme,
-            children: [],
-          });
-        }
+        return false;
+        // throw LiquidErrorInstance('Parser', `Invalid token \`${token.type}\``, token.start);
       }
-    }
-    return parseTree;
+    };
+
+    return build(input, ['EOF', this.grammar[0].lhs]);
   }
 }
